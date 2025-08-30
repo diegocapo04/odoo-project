bc29fa19318d5f75d7bc09b274218b70493a909c
diegocapo04 <diego.caporale04@gmail.com>
2025-08-30 09:28:41 +0200

    feat: seconda versione, aggiunta integrazione tra moduli.

--- addons/test_module/models/hr_employee.py
+++ addons/test_module/models/hr_employee.py
@@ -3,4 +3,10 @@ from odoo import models, fields
 class HrEmployee(models.Model):
     _inherit = 'hr.employee'
 
-    x_custom_note = fields.Char(string="Nota Personalizzata")
+    x_custom_note = fields.Char(string="Nota Personalizzata1")
+
+    task_ids = fields.One2many(
+        "project.task",
+        "employee_id",
+        string = "Task assegnate"
+    )
