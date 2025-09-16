926a0b8c995bd57b2303bf28c3a0581e263aeac4
diegocapo04 <diego.caporale04@gmail.com>
2025-09-16 14:52:14 +0200

    feat: cambiata integrazione da many2one a many2many

--- addons/test_module/models/project_task.py
+++ addons/test_module/models/project_task.py
@@ -3,9 +3,12 @@ from odoo import models, fields
 class ProjectTask(models.Model):
     _inherit = 'project.task'
 
-    x_custom_note = fields.Char(string="Nota Personalizzata0")
+    project_note = fields.Char(string="Note del team")
 
-    employee_id = fields.Many2one(
+    employee_ids = fields.Many2many(
         "hr.employee",
+        "task_employee_rel",
+        "task_id",
+        "employee_id",
         string = "Assegnato a Dipendente"
     )
